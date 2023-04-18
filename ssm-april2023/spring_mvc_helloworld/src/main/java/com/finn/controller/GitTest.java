82de8b84a4f08000f3b2e1f8a4715a342aaa77ac
finn <finn@gmail.com>
2023-04-18 18:02:51 +1000

    SSM April 2023, first commit. SpringMVC begins.

---
+++ ssm-april2023/spring_mvc_helloworld/src/main/java/com/finn/controller/GitTest.java
@@ -0,0 +1,7 @@
+package com.finn.controller;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
---
+++ ssm-april2023/spring_mvc_helloworld/src/main/java/com/finn/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.finn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//控制层组件
+@Controller
+public class HelloController {
+
+    //slash代表绝对路径，此处是服务器解析的绝对路径
+    @RequestMapping("/")
+    public String portal() {
+        //将逻辑视图返回（去掉物理视图的前后缀）
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "success";
+    }
+}
