3d8317e019950172c85037028e5ef4acf7641da8
finn <finn@gmail.com>
2023-04-18 22:35:11 +1000

    @RequestMapping的value属性（数组类型）。value属性是必须要设置的， 相当于一道多选题。

--- ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/PortalController.java
+++ ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/PortalController.java
@@ -1,6 +1,5 @@
 package com.finn.controller;
 
-import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
--- ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/TestRequestMappingController.java
+++ ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/TestRequestMappingController.java
@@ -4,15 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /*
+1.
     在基础路径下（上下文），加上/test/hello 就是要访问的地址
     相当有用！！
+2.
+    @RequestMapping的value属性
+    作用：通过请求的路径匹配请求。
+    value属性是数组类型，即当前浏览器所发送的请求路径匹配value属性中的任何一个值。
+    则当前请求就会被注解所标识的方法处理。
  */
 
 @Controller
 @RequestMapping("/test") //设置请求映射的基础信息
 public class TestRequestMappingController {
 
-    @RequestMapping("/hello") //设置请求映射的具体信息
+    @RequestMapping({"/hello", "/bennett"}) //设置请求映射的具体信息
     public String hello() {
         return "success";
     }
