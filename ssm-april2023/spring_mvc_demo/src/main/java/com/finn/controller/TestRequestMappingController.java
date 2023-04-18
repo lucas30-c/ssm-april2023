03f72471ecbcb4b39873cecd1d602368de12ebc9
finn <finn@gmail.com>
2023-04-18 22:16:19 +1000

    加上了success.html，并且在类上和方法上分别加入了@RequestMapping注解，指定了访问路径。
    此外，使用thymeleaf风格，<a th:href="@{/test/hello}"></a>指出了要跳转的地址。

--- ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/TestRequestMappingController.java
+++ ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/TestRequestMappingController.java
@@ -1,7 +1,19 @@
 package com.finn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+    在基础路径下（上下文），加上/test/hello 就是要访问的地址
+    相当有用！！
+ */
 
 @Controller
+@RequestMapping("/test") //设置请求映射的基础信息
 public class TestRequestMappingController {
+
+    @RequestMapping("/hello") //设置请求映射的具体信息
+    public String hello() {
+        return "success";
+    }
 }
