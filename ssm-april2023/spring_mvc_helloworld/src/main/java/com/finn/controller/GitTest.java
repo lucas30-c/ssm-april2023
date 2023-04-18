035044d467f2644c2e5ccbf9f45492cf3476f54b
finn <finn@gmail.com>
2023-04-18 18:07:02 +1000

    Hello git, 4 times.

--- ssm-april2023/spring_mvc_helloworld/src/main/java/com/finn/controller/GitTest.java
+++ ssm-april2023/spring_mvc_helloworld/src/main/java/com/finn/controller/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello git!");
         System.out.println("Hello git!");
         System.out.println("Hello git!");
+        System.out.println("Hello git!");
     }
 }
