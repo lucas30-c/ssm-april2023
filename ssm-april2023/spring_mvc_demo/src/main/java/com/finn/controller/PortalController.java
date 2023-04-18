036282664d535b57561421778d39b180a0c5a3b3
finn <finn@gmail.com>
2023-04-18 22:00:41 +1000

    Controller 下的index页面配置完毕，必须加上@Controller注解，否则报404.

--- ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/PortalController.java
+++ ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/PortalController.java
@@ -1,7 +1,10 @@
 package com.finn.controller;
 
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller //必须加注解，否则404
 public class PortalController {
 
     @RequestMapping("/")
