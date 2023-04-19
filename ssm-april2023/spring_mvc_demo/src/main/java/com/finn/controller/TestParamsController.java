66127f55c23120e3bbc5939cde0a73406afc589e
finn <finn@gmail.com>
2023-04-19 15:47:24 +1000

    4. @RequestHeader: 将请求头信息和控制器方法的形参绑定     5. @CookieValue: 将cookie数据和控制器方法的形参绑定
        掌握的还不太明白（cookie和session）

--- ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/TestParamsController.java
+++ ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/TestParamsController.java
@@ -1,10 +1,13 @@
 package com.finn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /*
     获取请求参数的方式：
@@ -27,6 +30,9 @@ import javax.servlet.http.HttpServletRequest;
     默认为true，表示value对应的请求参数必须传输，否则页面报错HTTP Status 400 – Bad Request
     若设置为false，则表示value对应的请求参数不是必须传输，如果未传输，则形参为null
     defaultValue：设置当没有传输value所对应的请求参数时，为形参设置的默认值。此时和required属性值无关。
+
+    4. @RequestHeader: 将请求头信息和控制器方法的形参绑定
+    5. @CookieValue: 将cookie数据和控制器方法的形参绑定
  */
 @Controller
 public class TestParamsController {
@@ -34,6 +40,7 @@ public class TestParamsController {
     @RequestMapping("/param/servletAPI")
     public String getParamsByServletAPI(HttpServletRequest request) {
         //回顾javaweb
+        HttpSession session = request.getSession();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         System.out.println("username: " + username + ", password: " + password);
@@ -41,7 +48,12 @@ public class TestParamsController {
     }
 
     @RequestMapping("/param")
-    public String getParam(@RequestParam(value = "userName", required = true) String username, String password) {
+    public String getParam(@RequestParam(value = "userName", required = true, defaultValue = "finn") String username,
+                           String password,
+                           @RequestHeader(value = "referer") String referer,
+                           @CookieValue("JSESSIONID") String jsessionId) {
+        System.out.println("jsessionId: " + jsessionId);
+        System.out.println("referer: " + referer);
         System.out.println("username: " + username + ", password: " + password);
         return "success";
     }
