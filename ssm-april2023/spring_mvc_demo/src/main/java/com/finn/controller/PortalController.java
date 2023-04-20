2a3394f3c61e1474b002f75e2baaf9016db011a3
finn <finn@gmail.com>
2023-04-20 19:01:52 +1000

    <!--    非常重要！！！开启mvc的注解驱动-->     <mvc:annotation-driven/>     <!--    视图控制器：为当前请求直接设置视图名称，实现页面跳转-->     <!--    如果设置视图控制器,则只有视图控制器所设置的请求会被处理,其他请求将全部404             此时必须设置一个标签<mvc:annotation-driven/>     -->     <mvc:view-controller path="/" view-name="index"></mvc:view-controller>

--- ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/PortalController.java
+++ ssm-april2023/spring_mvc_demo/src/main/java/com/finn/controller/PortalController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller //必须加注解，否则404
 public class PortalController {
 
-    @RequestMapping("/")
-    public String portal() {
-        return "index";
-    }
+//    @RequestMapping("/") //首页
+//    public String portal() {
+//        return "index";
+//    }
 
     @RequestMapping("/hello")
     public String success() {
