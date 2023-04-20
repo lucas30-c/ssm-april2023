5a36714714607b9be5e1349a41437524036f040c
finn <finn@gmail.com>
2023-04-20 21:05:12 +1000

    /**  * get 查询  * post 添加(增)  * delete 删除  * put 修改(更新)  *  * 查询所有的用户信息: /user->get    (一层)  * 根据id查询用户信息: /user/1->get (两层)  * 添加用户信息: /user->post  * 查询所有的用户信息: /user->get  * 修改用户信息: /user->put  * 删除用户信息: /user/1->delete  */

---
+++ ssm-april2023/spring_mvc_restful/src/main/java/com/finn/controller/TestRestController.java
@@ -0,0 +1,31 @@
+package com.finn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * get 查询
+ * post 添加(增)
+ * delete 删除
+ * put 修改(更新)
+ *
+ * 查询所有的用户信息: /user->get    (一层)
+ * 根据id查询用户信息: /user/1->get (两层)
+ * 添加用户信息: /user->post
+ * 查询所有的用户信息: /user->get
+ * 修改用户信息: /user->put
+ * 删除用户信息: /user/1->delete
+ */
+
+@Controller
+public class TestRestController {
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String getAllUsers() {
+        System.out.println("查询所有用户:" +
+                "@RequestMapping(value = \"/user\", method = RequestMethod.GET)");
+        return "success";
+    }
+
+}
