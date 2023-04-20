f93d1662484ee7f3da01de485e14137654affa39
finn <finn@gmail.com>
2023-04-20 23:15:37 +1000

    继续 web.xml: HiddenHttpMethodFilter
     * 注意: 浏览器目前只能发送get和post请求
     * 如果要发送put和delete请求,需要在web.xml中配置一个过滤器HiddenHttpMethodFilter
     * 配置了过滤器后,发送的请求要满足两个条件,才能将请求方式转化为put或delete
     * 1. 当前请求必须为post
     * 2. 当前请求必须传输请求参数_method,_method才是最终的请求方式.
     *
     * Spring Framework 提供了一个名为 HiddenHttpMethodFilter 的过滤器，
     * 可以让您使用一个隐藏的 _method 参数来指定实际的 HTTP 方法。
     * 这个过滤器会将请求重新包装为 PUT、DELETE 或其他 HTTP 方法，
     * 从而允许您在服务器端处理这些类型的请求。

--- ssm-april2023/spring_mvc_restful/src/main/java/com/finn/controller/TestRestController.java
+++ ssm-april2023/spring_mvc_restful/src/main/java/com/finn/controller/TestRestController.java
@@ -17,6 +17,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * 查询所有的用户信息: /user->get
  * 修改用户信息: /user->put
  * 删除用户信息: /user/1->delete
+ *
+ * 注意: 浏览器目前只能发送get和post请求
+ * 如果要发送put和delete请求,需要在web.xml中配置一个过滤器HiddenHttpMethodFilter
+ * 配置了过滤器后,发送的请求要满足两个条件,才能将请求方式转化为put或delete
+ * 1. 当前请求必须为post
+ * 2. 当前请求必须传输请求参数_method,_method才是最终的请求方式.
+ *
+ * Spring Framework 提供了一个名为 HiddenHttpMethodFilter 的过滤器，
+ * 可以让您使用一个隐藏的 _method 参数来指定实际的 HTTP 方法。
+ * 这个过滤器会将请求重新包装为 PUT、DELETE 或其他 HTTP 方法，
+ * 从而允许您在服务器端处理这些类型的请求。
  */
 
 @Controller
@@ -38,6 +49,18 @@ public class TestRestController {
     @RequestMapping(value="/user", method = RequestMethod.POST)
     public String insertUser() {
         System.out.println("添加用户信息");
+        return "success";
+    }
+
+    @RequestMapping(value="/user", method=RequestMethod.PUT)
+    public String updateUser() {
+        System.out.println("修改用户信息->/user->delete");
+        return "success";
+    }
+
+    @RequestMapping(value="/user/{id}", method = RequestMethod.DELETE)
+    public String deleteUser(@PathVariable("id") Integer id) {
+        System.out.println("删除用户信息->/user->put");
         return "success";
     }
 }
