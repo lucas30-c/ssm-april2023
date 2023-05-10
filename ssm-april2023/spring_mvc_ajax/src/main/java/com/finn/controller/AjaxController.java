0cf7c214037f453d754f40e7d09db73abc10e12b
lucas30-c <hopeful.dawn1999@gmail.com>
2023-05-10 17:40:52 +1000

    创建Spring+Ajax工程

--- ssm-april2023/spring_mvc_ajax/src/main/java/com/finn/controller/AjaxController.java
+++ ssm-april2023/spring_mvc_ajax/src/main/java/com/finn/controller/AjaxController.java
@@ -1,7 +1,17 @@
 package com.finn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class AjaxController {
+
+    @RequestMapping("test/ajax")
+    public void testAjax(Integer id, HttpServletResponse response) throws IOException {
+        System.out.println("id:" + id);
+        response.getWriter().write("hello, axios"); //涉及到流
+    }
 }
