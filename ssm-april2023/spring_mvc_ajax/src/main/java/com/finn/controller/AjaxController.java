fc78440b56fc1aa5da078304fcd2524fa0daf316
lucas30-c <hopeful.dawn1999@gmail.com>
2023-05-10 23:08:50 +1000

    5.10晚 再提交

--- ssm-april2023/spring_mvc_ajax/src/main/java/com/finn/controller/AjaxController.java
+++ ssm-april2023/spring_mvc_ajax/src/main/java/com/finn/controller/AjaxController.java
@@ -1,16 +1,20 @@
 package com.finn.controller;
 
+import com.finn.pojo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class AjaxController {
 
     @RequestMapping("test/ajax")
-    public void testAjax(Integer id, HttpServletResponse response) throws IOException {
+    public void testAjax(Integer id, HttpServletResponse response, @RequestBody String requestBody) throws IOException {
+        System.out.println("RequestBody: " + requestBody);
         System.out.println("id:" + id);
         response.getWriter().write("hello, axios"); //涉及到流
         /*
@@ -40,4 +44,23 @@ public class AjaxController {
         它可以读取响应的主体来获取服务器发送的数据（在这个例子中是"hello, axios"）。
 
      */
+
+    /*
+        使用@RequestBody注解将json格式的请求参数转换为java对象
+        a. 导入jackson依赖
+        b. 在SpringMVC的配置文件中设置<mvc:annotation-driven />
+        c. 在处理请求的控制器方法的形参位置，直接设置json格式的请求参数要转换的java类型的形参
+     */
+    @RequestMapping("/test/RequestBody/json")
+    public void testRequestBody(@RequestBody Map<String, Object> map, HttpServletResponse response) throws IOException {
+        System.out.println(map); //只打印有数据的部分
+        response.getWriter().write("Hello, RequestBody.");
+    }
+    //这两种写法基本等效
+
+//    @RequestMapping("/test/RequestBody/json")
+//    public void testRequestBody(@RequestBody User user, HttpServletResponse response) throws IOException {
+//        System.out.println(user);
+//        response.getWriter().write("Hello, RequestBody.");
+//    }
 }
---
+++ ssm-april2023/spring_mvc_ajax/src/main/java/com/finn/pojo/User.java
@@ -0,0 +1,75 @@
+package com.finn.pojo;
+
+public class User {
+    private Integer id;
+
+    private String userName;
+
+    private String password;
+
+    private Integer age;
+
+    private String gender;
+
+    public User() {
+    }
+
+    public User(Integer id, String userName, String password, Integer age, String gender) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
